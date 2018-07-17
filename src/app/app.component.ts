07d2181928c2ef9e3089a180af7476579fb9f321
pakoxtror <www.daniel_4_13@hotmail.com>
2018-07-17 15:24:18 -0500

    HomePage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) nav: Nav;
+  rootPage:any = LoginPage;
+  paginas: Array<{titulo: string, component: any}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +22,15 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    this.paginas = [
+      { titulo: 'Inicio', component: HomePage },
+      { titulo: 'Cerrar Sesión', component: LoginPage }
+    ];
+  }
+  abrirPagina(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
 
---
+++ src/app/paginas.ts
@@ -0,0 +1,7 @@
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+
+export const paginasList = [
+    HomePage,
+    LoginPage
+]
